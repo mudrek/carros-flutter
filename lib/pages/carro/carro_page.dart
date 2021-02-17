6e17b20ed246a14a54d8649d99f3e7d0097f3451
mudrek <giovanni.fernandes@livetouch.com.br>
2021-02-17 15:49:01 -0300

    140.

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -1,3 +1,4 @@
+import 'package:carros/widgets/text.dart';
 import 'package:flutter/material.dart';
 
 import 'carro.dart';
@@ -52,46 +53,49 @@ class CarroPage extends StatelessWidget {
       child: ListView(
         children: [
           Image.network(carro.urlFoto),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    carro.nome,
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    carro.tipo,
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    iconSize: 36,
-                    icon: Icon(Icons.favorite),
-                    onPressed: _onClickFavorito,
-                  ),
-                  IconButton(
-                    iconSize: 36,
-                    icon: Icon(Icons.share),
-                    onPressed: _onClickShare,
-                  ),
-                ],
-              ),
-            ],
+          _bloco1(),
+          Divider(
+            color: Colors.grey,
           ),
-
+          _bloco2(),
         ],
       ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            text(
+              carro.nome,
+              fontSize: 24,
+              bold: true,
+            ),
+            text(
+              carro.tipo,
+              fontSize: 16,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+              iconSize: 36,
+              icon: Icon(Icons.favorite),
+              onPressed: _onClickFavorito,
+            ),
+            IconButton(
+              iconSize: 36,
+              icon: Icon(Icons.share),
+              onPressed: _onClickShare,
+            ),
+          ],
+        ),
+      ],
     );
   }
 
@@ -111,9 +115,28 @@ class CarroPage extends StatelessWidget {
     }
   }
 
-  void _onClickFavorito() {
-  }
+  void _onClickFavorito() {}
 
-  void _onClickShare() {
+  void _onClickShare() {}
+
+  _bloco2() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 2,
+          ),
+          text("Descrição", fontSize: 20, bold: true),
+          SizedBox(
+            height: 12,
+          ),
+          text(
+              "Fogo é a rápida oxidação de um material combustível liberando calor, luz e produtos de reação, tais como o dióxido de carbono e a água.[1] O fogo é uma mistura de gases a altas temperaturas, formada em reação exotérmica de oxidação, que emite radiação eletromagnética nas faixas do infravermelho e visível. Desse modo, o fogo pode ser entendido como uma entidade gasosa emissora de radiação e decorrente da combustão. Se bastante quente, os gases podem se tornar ionizados para produzir plasma.[2] Dependendo das substâncias presentes e de quaisquer impurezas, a cor da chama e a intensidade do fogo podem variar. O fogo em sua forma mais comum pode resultar em incêndio, que tem o potencial de causar dano físico através da queima.",
+              fontSize: 16,
+              bold: false),
+        ],
+      ),
+    );
   }
 }
