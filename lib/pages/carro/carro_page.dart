05a9be80d6ec55f69852fe9a739e7bd654516a90
mudrek <giovanni.fernandes@livetouch.com.br>
2021-02-17 15:23:00 -0300

    138.

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -12,6 +12,35 @@ class CarroPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(carro.nome),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMapa,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickMapa,
+          ),
+          PopupMenuButton(
+            onSelected: (String value) => _onClickPopupMenu(value),
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: Text("Editar"),
+                  value: "Editar",
+                ),
+                PopupMenuItem(
+                  child: Text("Deletar"),
+                  value: "Deletar",
+                ),
+                PopupMenuItem(
+                  child: Text("Share"),
+                  value: "Share",
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: _body(),
     );
@@ -20,7 +49,71 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: EdgeInsets.all(16),
-      child: Image.network(carro.urlFoto),
+      child: ListView(
+        children: [
+          Image.network(carro.urlFoto),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    carro.nome,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    carro.tipo,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    iconSize: 36,
+                    icon: Icon(Icons.favorite),
+                    onPressed: _onClickFavorito,
+                  ),
+                  IconButton(
+                    iconSize: 36,
+                    icon: Icon(Icons.share),
+                    onPressed: _onClickShare,
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+        ],
+      ),
     );
+  }
+
+  void _onClickMapa() {}
+
+  _onClickPopupMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("Editar!!!!");
+        break;
+      case "Deletar":
+        print("Deletar!!!!");
+        break;
+      case "Share":
+        print("Share!!!!");
+        break;
+    }
+  }
+
+  void _onClickFavorito() {
+  }
+
+  void _onClickShare() {
   }
 }
